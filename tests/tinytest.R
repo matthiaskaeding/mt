2d76de2d3179ef485303fad8bc98febdf119aae1
Matthias <matthiaskaeding@posteo.de>
2020-11-02 10:17:26 +0100

    Correct error in test

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,1 +1,3 @@
-if (requireNamespace("tinytest", quietly=TRUE)){tinytest::test_package("PACKAGENAME")}
+if (requireNamespace("tinytest", quietly=TRUE)) {
+  tinytest::test_package("matthiastools")
+}
