8c930ea94752d3d2038b797f4fc1b77c620dab26
Matthias <matthiaskaeding@posteo.de>
2020-11-04 08:30:01 +0100

    Adding ext argument explanation

--- R/make_hive_folder.R
+++ R/make_hive_folder.R
@@ -3,6 +3,7 @@
 #' @param basefolder Basic folder
 #' @param ... Key value pairs. Specify this OR pairs
 #' @param pairs Named list of key values parirs; etc. day=01
+#' @param ext File extension; e.g. '.fst'.
 #' @keywords hive_folder
 #' @export
 #' @examples
