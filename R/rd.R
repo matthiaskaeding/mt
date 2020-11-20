423b32642bf7dfd3f9daaaa9f93b00bf51f15f7f
Matthias <matthiaskaeding@posteo.de>
2020-11-20 10:30:57 +0100

    new function zd; unlock parameter for rd

--- R/rd.R
+++ R/rd.R
@@ -2,12 +2,13 @@
 #' Simple helper to avoid the need to create run.R
 #'
 #' @keywords drake
+#' @param unluck Unlock cache before running drake::r_make
 #' @export
 
-rd <- function() {
+rd <- function(unlock = TRUE) {
 
   source(here::here("_drake.R"))
-  drake::drake_cache(here::here(".drake"))$unlock()
+  if(unlock) drake::drake_cache(here::here(".drake"))$unlock()
   drake::r_make()
 
 }
---
+++ R/zd.R
@@ -0,0 +1,8 @@
+#' Source _drake.R file (name sd was already taken :b)
+#'
+#' @keywords drake
+#' @export
+
+zd <- function() {
+  source(here::here("_drake.R"))
+}
