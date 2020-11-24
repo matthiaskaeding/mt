1c289455657adb5fac800945ee973e4779bc80b7
Matthias <matthiaskaeding@posteo.de>
2020-11-24 14:43:53 +0100

    slight correction to "include.only" argument

--- inst/templates/packages.R
+++ inst/templates/packages.R
@@ -1,7 +1,7 @@
 options(conflicts.policy = list(error = TRUE, warn = FALSE))
 
 library(dplyr, mask.ok = "filter",
-        include = c("select", "filter", "slice", "mutate", "summarize"))
+        include.only = c("select", "filter", "slice", "mutate", "summarize"))
 
 library(drake)
 library(here)
