51f1b0cf2b7f59f0c917366a826b57b998a66197
Matthias <matthiaskaeding@posteo.de>
2020-11-26 09:54:03 +0100

    Change in _drake.R default file: Source all R files except those starting with dot

--- inst/templates/_drake.R
+++ inst/templates/_drake.R
@@ -1,4 +1,8 @@
-source(here::here("R/packages.R"))
-source(here::here("R/plan.R"))
+# Source all files in R, omitting files starting with dot.
+R_files <- list.files(
+  here::here("R"), pattern = "\\.R$", all.files = FALSE, full.names = TRUE
+  )
+
+lapply(R_files, source)
 
 drake::drake_config(plan)
