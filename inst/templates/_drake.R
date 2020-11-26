7caeaf6464eb8cc627d55ba24b917b7de05c1e0e
Matthias <matthiaskaeding@posteo.de>
2020-11-26 10:05:05 +0100

    Same as last commit; more explicit

--- inst/templates/_drake.R
+++ inst/templates/_drake.R
@@ -1,8 +1,9 @@
-# Source all files in R, omitting files starting with dot.
+# Look up all .R files, omit those starting with dot
 R_files <- list.files(
-  here::here("R"), pattern = "\\.R$", all.files = FALSE, full.names = TRUE
-  )
+  here::here("R"), pattern = "\\.R$", all.files = TRUE, full.names = TRUE)
 
-lapply(R_files, source)
+lapply(
+  R_files[!startsWith(basename(R_files), ".")],
+  source)
 
 drake::drake_config(plan)
