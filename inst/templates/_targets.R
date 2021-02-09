6906ba82b1b47015c7a05f2b098e4e827e71dcda
Matthias <matthiaskaeding@posteo.de>
2021-02-09 18:22:02 +0100

    Omit the startswith stuff, which wouldnt have worked

--- inst/templates/_targets.R
+++ inst/templates/_targets.R
@@ -5,8 +5,10 @@ library(data.table)
 library(ggplot2)
 library(purrr)
 
-files <- list.files(here("R"), pattern = "\\.R$", full.names = TRUE)
-lapply(files[!startsWith(files, ".")], source)
+lapply(
+  list.files(here("R"), pattern = "\\.R$", full.names = TRUE, all.files = FALSE),
+  source
+)
 
 c(
   ### Write targets here
