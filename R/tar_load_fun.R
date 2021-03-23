9fbaf8d9016941df9e29b369ed64c0f0b39edf69
Matthias <matthiaskaeding@posteo.de>
2021-03-23 15:33:42 +0100

    Correct error in function name

--- R/tar_load_fun.R
+++ R/tar_load_fun.R
@@ -5,7 +5,7 @@
 #' @param fun function, whose arguments we want to loadd
 #' @export
 
-loadd_fun <- function(fun, envir = parent.frame()) {
+tar_load_fun <- function(fun, envir = parent.frame()) {
 
   sargs <- names(as.list(args(fun)))
   sargs <- setdiff(sargs, c("", "..."))
