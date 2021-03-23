86ca3834c3e7ad3e40d0769a7acf067bc4a26241
Matthias <matthiaskaeding@posteo.de>
2021-03-23 14:16:08 +0100

    New function 'tar_load_fun'

---
+++ R/tar_load_fun.R
@@ -0,0 +1,14 @@
+#' targets::tar_loads all arguments of function
+#' Common usecase: tar_load all objects while debugging a make_<object> function
+#'
+#' @keywords drake
+#' @param fun function, whose arguments we want to loadd
+#' @export
+
+loadd_fun <- function(fun, envir = parent.frame()) {
+
+  sargs <- names(as.list(args(fun)))
+  sargs <- setdiff(sargs, c("", "..."))
+  targets::tar_load_raw(sargs, envir = envir)
+
+}
